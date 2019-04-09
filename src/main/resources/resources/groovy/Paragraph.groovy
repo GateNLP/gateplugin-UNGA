2830d35b9baca54f39ed639c2fbd594074e40bba
Adam Funk <a.funk@sheffield.ac.uk>
2019-04-09 18:06:56 +0100

    more headings; date normalizer

--- src/main/resources/resources/groovy/Paragraph.groovy
+++ src/main/resources/resources/groovy/Paragraph.groovy
@@ -1,9 +1,6 @@
 
-AnnotationSet paragraphs = doc.getAnnotations("Original markups").get("p");
-AnnotationSet spaces = inputAS.get("SpaceToken");
-
-for (Annotation space : spaces) {
-    if (Utils.getCoveringAnnotations(paragraphs, space).isEmpty()) {
-        Utils.addAnn(outputAS, space, "PBreak", Factory.newFeatureMap());
-    }
-}
+matcher = content =~ "\\n(\\s\\n)+";
+while(matcher.find()) {
+  outputAS.add(matcher.start(), matcher.end(), 
+               "PBreak", Factory.newFeatureMap());
+               }
