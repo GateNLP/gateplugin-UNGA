b821e843e2c2271590743d7f15f43462a7d9cc24
Adam Funk <a.funk@sheffield.ac.uk>
2019-04-11 10:25:45 +0100

    Exclusions and whitespace cleaning

--- bin/unbist2gazetter.py
+++ bin/unbist2gazetter.py
@@ -3,6 +3,9 @@ import argparse
 import rdflib.plugins.sparql
 from rdflib.namespace import NamespaceManager
 
+exclusions = ["THE"]
+
+
 oparser = argparse.ArgumentParser(description="consolidate CVS output to one line per document",
                                   formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
@@ -23,9 +26,10 @@ ns_manager = NamespaceManager(rdflib.Graph())
 ns_manager.bind('skos', rdflib.namespace.SKOS)
 graph.namespace_manager = ns_manager
 
+print('Reading', options.input_file)
 graph.parse(options.input_file, format='n3')
 
-print('Triples:', len(graph))
+print('Triples loaded:', len(graph))
 
 prefix_string = 'PREFIX skos: <%s>' % rdflib.namespace.SKOS
 
@@ -48,23 +52,30 @@ query = rdflib.plugins.sparql.prepareQuery('%s SELECT DISTINCT ?x WHERE {?x a sk
 
 nbr_concepts = 0
 nbr_alt_labels = 0
+nbr_excluded = 0
+
+print('Generating', options.output_file)
 
 with open(options.output_file, 'w', encoding='utf-8') as f:
     for row in graph.query(query):
         concept = row[0]
         query1 = rdflib.plugins.sparql.prepareQuery(pref_label_query % concept)
         for row1 in graph.query(query1):
-            pref_label = row1[0]
+            pref_label = row1[0].strip()
             continue
         f.write('\t'.join([pref_label, 'pref_label=%s' % pref_label, 'uri=%s' % concept]) + '\n')
         nbr_concepts += 1
 
         query2 = rdflib.plugins.sparql.prepareQuery(alt_label_query % concept)
         for row2 in graph.query(query2):
-            label = row2[0]
-            f.write('\t'.join([label, 'pref_label=%s' % pref_label, 'uri=%s' % concept]) + '\n')
-            nbr_alt_labels += 1
+            label = row2[0].strip()
+            if label in exclusions:
+                nbr_excluded += 1
+            else:
+                f.write('\t'.join([label, 'pref_label=%s' % pref_label, 'uri=%s' % concept]) + '\n')
+                nbr_alt_labels += 1
 
 print('Number of concepts:', nbr_concepts)
 print('Number of alt labels:', nbr_alt_labels)
+print('Number of alt labels excluded:', nbr_excluded)
 print('Lines written:' , nbr_concepts + nbr_alt_labels)
