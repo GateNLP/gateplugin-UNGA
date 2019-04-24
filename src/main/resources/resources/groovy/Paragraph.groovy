4ba5fa625f4b7a6b58b5937d99e5041dc8131b97
Adam Funk <a.funk@sheffield.ac.uk>
2019-04-24 09:30:21 +0100

    avoid failing on empty document content

--- src/main/resources/resources/groovy/Paragraph.groovy
+++ src/main/resources/resources/groovy/Paragraph.groovy
@@ -1,22 +1,23 @@
 
 end = Utils.lengthLong(doc);
-outputAS.add(end - 1L, end, "DocEnd", Factory.newFeatureMap());
 
-matcher = content =~ "\\n(\\s\\n)+\\s*";
-while(matcher.find()) {
-  if ( outputAS.getCovering("DocEnd", matcher.start(), matcher.end()).isEmpty() &&
-       outputAS.get("DocEnd", matcher.start(), matcher.end()).isEmpty() ) {
-      outputAS.add(matcher.start(), matcher.end(), 
+if (end > 0L) {
+    outputAS.add(end - 1L, end, "DocEnd", Factory.newFeatureMap());
+
+    matcher = content =~ "\\n(\\s\\n)+\\s*";
+    while(matcher.find()) {
+      if ( outputAS.getCovering("DocEnd", matcher.start(), matcher.end()).isEmpty() &&
+           outputAS.get("DocEnd", matcher.start(), matcher.end()).isEmpty() ) {
+          outputAS.add(matcher.start(), matcher.end(), 
                    "MajorBreak", Factory.newFeatureMap());
-  }
+      }
+    }
+
+    matcher = content =~ "\\n\\s+";
+    while(matcher.find()) {
+      if (outputAS.getCovering("MajorBreak", matcher.start(), matcher.end()).isEmpty()) {
+          outputAS.add(matcher.start(), matcher.end(), 
+                       "MinorBreak", Factory.newFeatureMap());
+      }
+    }
 }
-
-matcher = content =~ "\\n\\s+";
-while(matcher.find()) {
-  if (outputAS.getCovering("MajorBreak", matcher.start(), matcher.end()).isEmpty()) {
-      outputAS.add(matcher.start(), matcher.end(), 
-                   "MinorBreak", Factory.newFeatureMap());
-  }
-}
-
-
